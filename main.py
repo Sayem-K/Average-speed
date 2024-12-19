37aac6488b2fc37f7d960127fdd1438d48fa85b5
hoigoysohio <njuli@yahoo.com>
2024-12-19 18:52:10 +0000

    done

---
+++ main.py
@@ -0,0 +1,11 @@
+speed_1 = 10
+speed_2 = 20
+speed_3 = 30
+
+average = (speed_1 + speed_2 + speed_3) / 3
+if speed_1 < average:
+    print("Speed 1 is slower with the difference of" ,average - speed_1)
+if speed_2 < average:
+    print("Speed 2 is slower with the difference of" ,average - speed_2)
+if speed_3 < average:
+    print("Speed 3 is slower with the difference of" ,average - speed_3)
